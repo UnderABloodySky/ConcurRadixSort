67fed750b2dbf72042f622af93f9b265c7f73abf
matias <matias@DESKTOP-VAFV3S6>
2019-06-02 18:37:15 -0300

    raid y split casi terminado,faltaria el error del tipo de mask

--- src/radixSort/ConcurRadixSort.java
+++ src/radixSort/ConcurRadixSort.java
@@ -1,5 +1,8 @@
 package radixSort;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 import threadPool.ThreadPool;
 
 public class ConcurRadixSort {
@@ -8,7 +11,40 @@ public class ConcurRadixSort {
         myThreadPool = new ThreadPool(bufferSize,quantityThreads);
     }
 
-    public int[] radixSort(int[] listToSort){
-        return listToSort;
-    }
+    
+//    La lista no importa el largo q tenga mientras los numeros se representen
+//    en 32 bits
+    public int[] radixSort ( int[] listToSort ) {
+    	int[]result= {};
+    	for (int i = 0; i < 32; ++i) {
+    	int [][] aux = this.split(listToSort , i);
+    	int[] ones=aux[1];
+    	int[] zeros=aux[0];
+    	result = IntStream.concat(Arrays.stream(zeros),Arrays.stream(ones)).toArray();//aux[0].concat(aux[1]);
+    	}
+    	return result ;
+    	}
+    
+    public int[][]	split (int[] listToSplit ,int i) {
+    	int[] zeros = {};
+    	int[] ones = {};
+    	int[][] result= {};
+    	int mask = 1 << i;
+    	int countDeOnes=0;
+    	int countDeZeros=0;
+    	for (int num:listToSplit ) {
+    	if (num & mask ) {
+
+    		ones[countDeOnes]=num;
+    		countDeOnes++;
+    		}
+    	else {
+    		zeros[countDeZeros]=num;
+    		countDeZeros++;
+    		}
+    	}
+    	result[1]=ones;
+    	result[0]=zeros;
+    	return result;
+    	}
 }
