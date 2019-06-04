49a3c4a67a9845633158ec111b3b1c5d750d981c
matias <matias@DESKTOP-VAFV3S6>
2019-06-04 00:37:42 -0300

    mensaje raid andando,es para un solo thread por ahora

--- src/radixSort/ConcurRadixSort.java
+++ src/radixSort/ConcurRadixSort.java
@@ -1,9 +1,9 @@
 package radixSort;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+//import java.util.Arrays;
 import java.util.List;
-import java.util.stream.IntStream;
+//import java.util.stream.IntStream;
 
 import threadPool.ThreadPool;
 
@@ -17,21 +17,22 @@ public class ConcurRadixSort {
     
 //    La lista no importa el largo q tenga mientras los numeros se representen
 //    en 32 bits
-    public int[] radixSort ( int[] listToSort ) {
-    	int[]result= {};
+    public List<Integer> radixSort ( List<Integer> listToSort ) {
+    	List<Integer> result= listToSort;
     	for (int i = 0; i < 32; ++i) {
-    	int [][] aux = this.split(listToSort , i);
-    	int[] ones=aux[1];
-    	int[] zeros=aux[0];
-    	result = IntStream.concat(Arrays.stream(zeros),Arrays.stream(ones)).toArray();//aux[0].concat(aux[1]);
+//    	int [][] aux = this.split(listToSort , i);cambio desde aca
+    	result=this.split(result, i);	
+//    	int[] ones=aux[1];
+//    	int[] zeros=aux[0];
+//    	result = IntStream.concat(Arrays.stream(zeros),Arrays.stream(ones)).toArray();//aux[0].concat(aux[1]);
     	}
     	return result ;
     	}
     
-    public List<List<Integer>>	split (List<Integer> listToSplit ,int i) {
+    public List<Integer>	split (List<Integer> listToSplit ,int i) {
     	List<Integer> zeros = new ArrayList<Integer>();
     	List<Integer> ones = new ArrayList<Integer>();
-    	List<List<Integer>> result= new ArrayList<>();
+    	List<Integer> result= new ArrayList<Integer>();
     	int mask = 1 << i;
 //    	int countDeOnes=0;
 //    	int countDeZeros=0;
@@ -47,8 +48,8 @@ public class ConcurRadixSort {
     		}
     	}
     	
-    	result.add(0, zeros);
-    	result.add(1, ones);
+    	result.addAll( zeros);
+    	result.addAll( ones);
     	return result;
     	}
 //    
--- test/radixSort/TestConcurRadixSort.java
+++ test/radixSort/TestConcurRadixSort.java
@@ -11,14 +11,14 @@ import org.junit.Test;
 public class TestConcurRadixSort {
 	private ConcurRadixSort concurPrueba;
 	private List<Integer> listaAOrdenar;
-	private List<Integer> listaCeroOrdenada;
+//	private List<Integer> listaCeroOrdenada;
 	private List<Integer> listaUnoOrdenada;
-	private List<List<Integer>> listaConListasCeroUno;
+	private List<Integer> listaConListasCeroUno;
 	@Before
 	public void setup() {
 	concurPrueba=new ConcurRadixSort(0, 0);	
 	listaAOrdenar = new ArrayList<Integer>();
-	listaCeroOrdenada= new ArrayList<Integer>();
+//	listaCeroOrdenada= new ArrayList<Integer>();
 	listaUnoOrdenada= new ArrayList<Integer>();
 	listaConListasCeroUno= new ArrayList<>();
 	}
@@ -28,20 +28,32 @@ public class TestConcurRadixSort {
 		listaAOrdenar.add(7);
 		listaAOrdenar.add(4);
 		listaAOrdenar.add(3);
-		
 //		listaCeroOrdenada.add(6);
-
 //		listaCeroOrdenada.add(4);
 		listaUnoOrdenada.add(6);
-		listaUnoOrdenada.add(7);
-//		listaUnoOrdenada.add(3);
 		listaUnoOrdenada.add(4);
+		listaUnoOrdenada.add(7);
+		listaUnoOrdenada.add(3);
+				
+		listaConListasCeroUno=concurPrueba.split(listaAOrdenar, 0);
 		
-		
-		listaConListasCeroUno=concurPrueba.split(listaAOrdenar, 2);
-		
-
-		assertEquals(listaUnoOrdenada ,listaConListasCeroUno.get(1));
+		assertEquals(listaUnoOrdenada ,listaConListasCeroUno);
 	}
 
+	@Test
+	public void testRaidUsandoSplit() {
+		listaAOrdenar.add(6);
+		listaAOrdenar.add(7);
+		listaAOrdenar.add(4);
+		listaAOrdenar.add(3);
+		
+		listaUnoOrdenada.add(3);
+		listaUnoOrdenada.add(4);
+		listaUnoOrdenada.add(6);
+		listaUnoOrdenada.add(7);
+		
+		
+		assertEquals(listaUnoOrdenada,concurPrueba.radixSort(listaAOrdenar));
+	}
+	
 }
