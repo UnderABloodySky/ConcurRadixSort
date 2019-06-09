d495897669ebf3d1f6f9c28395bdf4a2a081bd3f
matias <matias@DESKTOP-VAFV3S6>
2019-06-08 23:37:36 -0300

    version para mandar

--- src/radixSort/MainConcurRadixSort.java
+++ src/radixSort/MainConcurRadixSort.java
@@ -1,4 +1,45 @@
 package radixSort;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainConcurRadixSort {
+    private static ConcurRadixSort radix;
+    private static int quantityThreads;
+    private static List<Integer> toOrder;
+
+    public static void main(String[] args){
+       toOrder = generateListToOrder();
+       System.out.println("Original List: ");
+       System.out.println("");
+       for(Integer elem : toOrder){
+           System.out.println(elem);
+       }
+       System.out.println("");
+       System.out.println("_________________________________________________________________________");
+       System.out.println("");
+       quantityThreads = 2;
+       radix = new ConcurRadixSort(4, quantityThreads);
+       radix.radixSort(toOrder);
+       System.out.println("Ordened List: ");
+       for(Integer elem : toOrder){
+            System.out.println(elem);
+        }
+    }
+
+    private static List<Integer> generateListToOrder() {
+        List<Integer> aux = new ArrayList<>();
+       /*
+        for (int i = 0; i < 10000; ++i) {
+           int elem = (int) (Math.random() * 10000);
+           aux.add(elem);
+        }
+        */
+
+        aux.add(4);
+        aux.add(3);
+        aux.add(2);
+        aux.add(1);
+        return aux;
+    }
 }
--- test/radixSort/TestConcurRadixSort.java
+++ test/radixSort/TestConcurRadixSort.java
@@ -16,44 +16,59 @@ public class TestConcurRadixSort {
 	private List<Integer> listaConListasCeroUno;
 	@Before
 	public void setup() {
-	concurPrueba=new ConcurRadixSort(0, 0);	
+	concurPrueba=new ConcurRadixSort(2, 2);	
 	listaAOrdenar = new ArrayList<Integer>();
 //	listaCeroOrdenada= new ArrayList<Integer>();
 	listaUnoOrdenada= new ArrayList<Integer>();
-	listaConListasCeroUno= new ArrayList<>();
+	listaConListasCeroUno= new ArrayList<Integer>();
 	}
+//	@Test
+//	public void testSplit() {
+//		listaAOrdenar.add(6);
+//		listaAOrdenar.add(7);
+//		listaAOrdenar.add(4);
+//		listaAOrdenar.add(3);
+////		listaCeroOrdenada.add(6);
+////		listaCeroOrdenada.add(4);
+//		listaUnoOrdenada.add(6);
+//		listaUnoOrdenada.add(4);
+//		listaUnoOrdenada.add(7);
+//		listaUnoOrdenada.add(3);
+//				
+//		listaConListasCeroUno=concurPrueba.split(listaAOrdenar, 0);
+//		
+//		assertEquals(listaUnoOrdenada ,listaConListasCeroUno);
+//	}
+//
+//	@Test
+//	public void testRaidUsandoSplit() {
+//		listaAOrdenar.add(6);
+//		listaAOrdenar.add(7);
+//		listaAOrdenar.add(4);
+//		listaAOrdenar.add(3);
+//		
+//		listaUnoOrdenada.add(3);
+//		listaUnoOrdenada.add(4);
+//		listaUnoOrdenada.add(6);
+//		listaUnoOrdenada.add(7);
+//		
+//		
+//		assertEquals(listaUnoOrdenada,concurPrueba.radixSort(listaAOrdenar));
+//	}
+	
 	@Test
-	public void testSplit() {
+	public void testConcur() {
 		listaAOrdenar.add(6);
 		listaAOrdenar.add(7);
 		listaAOrdenar.add(4);
 		listaAOrdenar.add(3);
 //		listaCeroOrdenada.add(6);
 //		listaCeroOrdenada.add(4);
-		listaUnoOrdenada.add(6);
-		listaUnoOrdenada.add(4);
-		listaUnoOrdenada.add(7);
-		listaUnoOrdenada.add(3);
-				
-		listaConListasCeroUno=concurPrueba.split(listaAOrdenar, 0);
-		
-		assertEquals(listaUnoOrdenada ,listaConListasCeroUno);
-	}
-
-	@Test
-	public void testRaidUsandoSplit() {
-		listaAOrdenar.add(6);
-		listaAOrdenar.add(7);
-		listaAOrdenar.add(4);
-		listaAOrdenar.add(3);
-		
 		listaUnoOrdenada.add(3);
 		listaUnoOrdenada.add(4);
 		listaUnoOrdenada.add(6);
 		listaUnoOrdenada.add(7);
-		
-		
+//		listaConListasCeroUno= 
 		assertEquals(listaUnoOrdenada,concurPrueba.radixSort(listaAOrdenar));
 	}
-	
 }
